38ffbe220cf12659555727ac938f07cf8d273c63
Dylan Ratcliffe <dylan.ratcliffe@puppet.com>
2017-09-15 10:13:38 +1000

    more tracing

--- lib/puppet/parser/functions/noop.rb
+++ lib/puppet/parser/functions/noop.rb
@@ -2,19 +2,19 @@
 Puppet::Parser::Functions::newfunction(:noop, :arity => 1) do |args|
   raise(Puppet::ParseError, "noop(): Requires an array") unless args[0].is_a? Array
 
-  # class << resource('Class','role::test')
-  #   def lookupdefaults(type)
-  #     values = super(type)
-  #
-  #     # Create a new :noop parameter with the specified value (true/false) for our defaults hash
-  #     noop = Puppet::Parser::Resource::Param.new(
-  #       :name => :noop, :value => true, :source => self.source )
-  #
-  #     # Replace whatever defaults we recieved
-  #     values[:noop] = noop
-  #     values
-  #   end
-  # end
+  class << resource('Class','role::test')
+    # def lookupdefaults(type)
+    #   values = super(type)
+    #
+    #   # Create a new :noop parameter with the specified value (true/false) for our defaults hash
+    #   noop = Puppet::Parser::Resource::Param.new(
+    #     :name => :noop, :value => true, :source => self.source )
+    #
+    #   # Replace whatever defaults we recieved
+    #   values[:noop] = noop
+    #   values
+    # end
+  end
 
   require 'pry'
   binding.pry
